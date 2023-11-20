d043ed1f6d9c00c89c2196dda04874d43f088550
Zac <mymengyu@gmail.com>
2023-11-20 18:30:09 +0800

    chore: Update dependencies and Rust version
    - Add features key to clap dependency in Cargo.toml
    - Update Rust version in .tool-versions to 1.74.0

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,39 @@
 use std::{fs::File, io::{BufRead, BufReader}};
-use clap::{App, Arg};
+use clap::{Parser, Arg};
 use jsonwebtoken::{decode, Algorithm, Validation};
 
+#[derive(Parser)]
+#[command(author, version)]
+#[command(about = "stringer - a simple CLI to transform and inspect strings", long_about = "stringer is a super fancy CLI (kidding)")]
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Reverses a string
+    Info(Info),
+    /// Inspects a string
+    Crack(Crack),
+}
+
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Args)]
+struct Info {
+    /// The string to info
+    string: Option<String>,
+}
+
+#[derive(Args)]
+struct Crack {
+    /// The string to crack
+    string: Option<String>,
+    #[arg(short = 'd', long = "digits")]
+    only_digits: bool,
+}
+
+
 fn main() {
-    let matches = App::new("jvvt")
-        .version("1.0")
-        .author("7a6136")
-        .about("Crack JWT using dictionary attack")
-        .arg(Arg::new("token")
-            .short('t')
-            .long("token")
-            .value_name("TOKEN")
-            .about("Sets the JWT token")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::new("file")
-            .short('f')
-            .long("file")
-            .value_name("FILE")
-            .about("Sets the dictionary file")
-            .takes_value(true)
-            .required(true))
-        .get_matches();
-
-    let token = matches.value_of("token").unwrap();
-    let dictionary_file = matches.value_of("file").unwrap();
-
-    let file = File::open(dictionary_file).expect("無法打開檔案");
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let secret = line.expect("無法讀取行");
-        let validation = Validation::new(Algorithm::HS256, secret.as_bytes());
-
-        match decode::<serde_json::Value>(&token, secret.as_bytes(), &validation) {
-            Ok(_) => {
-                println!("找到正確的密鑰: {}", secret);
-                break;
-            },
-            Err(_) => continue,
-        }
-    }
+    let cli = Cli::parse();
 }
